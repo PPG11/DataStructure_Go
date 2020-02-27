0e7bd69645eea34c41548effea023551bf9bf631
风铃 <luolie0577@126.com>
2020-02-27 23:29:11 +0800

    version 1.1

--- Cpt_1/Max2.go
+++ Cpt_1/Max2.go
@@ -7,7 +7,7 @@ func main() {
 	var x1, x2 int
 	//x1, x2 = new(int), new(int)
 	max2(data, 0, 9, &x1, &x2)
-	fmt.Println("x1, x2 = ", x1, x2)
+	fmt.Println("x1, x2 =", x1, x2)
 	fmt.Println("max 2 data:", data[x1], data[x2])
 }
 
@@ -39,26 +39,25 @@ func max2(data []int, lo int, hi int, x1 *int, x2 *int) {
 	var x1l, x2l, x1r, x2r int
 	mi := (lo + hi) >> 1
 	max2(data, lo, mi, &x1l, &x2l)
+	fmt.Printf("left [%d, %d): x1l=%d, x2l=%d\n", lo, mi, x1l, x2l)
 	max2(data, mi, hi, &x1r, &x2r)
-	fmt.Printf("max [%d, %d): x1l=%d, x2l=%d\n", lo, mi, data[x1l], data[x2l])
-	fmt.Printf("max [%d, %d): x1r=%d, x2r=%d\n", mi, hi, data[x1r], data[x2r])
+	fmt.Printf("right [%d, %d): x1r=%d, x2r=%d\n", mi, hi, x1r, x2r)
 
-	fmt.Println("------ before if ------")
 	fmt.Printf("x1l = %d, x1r = %d, x2l = %d, x2r = %d, x1 = %d, x2 = %d\n", x1l, x1r, x2l, x2r, *x1, *x2)
+	fmt.Println("------ after combine l & r ------")
 	if data[x1l] > data[x1r] {
-
-		x1 = &x1l
+		*x1 = x1l
 		if data[x2l] > data[x1r] {
-			x2 = &x2l
+			*x2 = x2l
 		} else {
-			x2 = &x1r
+			*x2 = x1r
 		}
 	} else {
-		x1 = &x1r
+		*x1 = x1r
 		if data[x1l] > data[x2r] {
-			x2 = &x1l
+			*x2 = x1l
 		} else {
-			x2 = &x2r
+			*x2 = x2r
 		}
 	}
 	fmt.Printf("x1l = %d, x1r = %d, x2l = %d, x2r = %d, x1 = %d, x2 = %d\n", x1l, x1r, x2l, x2r, *x1, *x2)
