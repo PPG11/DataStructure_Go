4c9ea5ed77a5d5f611f33c606f8bd74b24b77e4b
风铃 <luolie0577@126.com>
2020-02-28 17:05:49 +0800

    version 1

--- Cpt_1/LCS_bad.go
+++ Cpt_1/LCS_bad.go
@@ -2,24 +2,25 @@ package main
 
 import "fmt"
 
+// longest common subsequence
 func main() {
 	str1 := "didactically"
 	str2 := "advantage"
-	longest := LCS(str1, str2)
+	longest := LCS1(str1, str2)
 	fmt.Println("the length of the longest common subsequence is", longest)
 }
 
-func LCS(str1 string, str2 string) int {
-	fmt.Println("---")
-	fmt.Println("str1 =", str1)
-	fmt.Println("str2 =", str2)
+func LCS1(str1 string, str2 string) int {
+	//fmt.Println("---")
+	//fmt.Println("str1 =", str1)
+	//fmt.Println("str2 =", str2)
 	switch {
 	case len(str1) == 0 || len(str2) == 0:
 		return 0
 	case str1[0] == str2[0]:
-		return LCS(str1[1:], str2[1:]) + 1
+		return LCS1(str1[1:], str2[1:]) + 1
 	default:
-		return max(LCS(str1[1:], str2), LCS(str1, str2[1:]))
+		return max(LCS1(str1[1:], str2), LCS1(str1, str2[1:]))
 	}
 }
 
---
+++ Cpt_1/LCS_dp.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// longest common subsequence
+var LCSMatrix [][]int
+
+func main() {
+	str1 := "didactically"
+	str2 := "advantage"
+	n1, n2 := len(str1), len(str2)
+	LCSMatrix = make([][]int, n1+1)
+	for i := 0; i <= n1; i++ {
+		LCSMatrix[i] = make([]int, n2+1)
+	}
+	LCSdp(str1, str2)
+	longest := LCSMatrix[n1][n2]
+	for i := 0; i <= n1; i++ {
+		fmt.Println(LCSMatrix[i])
+	}
+	fmt.Println("the length of the longest common subsequence is", longest)
+}
+
+func LCSdp(str1 string, str2 string) {
+	n1, n2 := len(str1), len(str2)
+	for i := 1; i <= n1; i++ {
+		for j := 1; j <= n2; j++ {
+			if str1[i-1] == str2[j-1] {
+				LCSMatrix[i][j] = LCSMatrix[i-1][j-1] + 1
+			} else {
+				LCSMatrix[i][j] = maxDP(LCSMatrix[i][j-1], LCSMatrix[i-1][j])
+			}
+		}
+	}
+}
+
+func maxDP(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
